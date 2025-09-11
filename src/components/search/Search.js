9a9bde16a257d51597cdf20c043eb11b851ad32d
MaSidrenko <nexon863@gmail.com>
2025-09-11 21:43:03 +0300

    Pagination in Progress

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -5,19 +5,47 @@ import { type } from "@testing-library/user-event/dist/type";
 class Seacrh extends React.Component {
     state = {
         search: "",
-        // type: "all"
+        type: "all",
+        page: 1
+    }
+    prevPage = () => {
+        this.setState
+            (
+                () => (this.state.page > 1 ? { page: this.state.page - 1 } : { page: 1 }),
+                () => { this.props.searchMovie(this.state.search, this.state.type, this.state.page) }
+            )
+    }
+    nextPage = () => {
+        this.setState(
+            () => ({ page: this.state.page + 1 }),
+            () => { this.props.searchMovie(this.state.search, this.state.type, this.state.page) }
+        )
     }
     handleKey = (e) => {
         if (e.key === 'Enter')
-            this.props.searchMovie(this.state.search, this.state.type);
+            this.props.searchMovie(this.state.search , this.state.type);
     }
     handleFilter = (e) => {
         this.setState(
-            () => ({type:e.target.dataset.type}),
-            () => {this.props.searchMovie(this.state.search, this.state.type)}
+            () => ({ type: e.target.dataset.type }),
+            () => { this.props.searchMovie(this.state.search, this.state.type) }
+        );
+    }
+    setPage = (num) => {
+        this.setState(
+            () => ({ page: num }),
+            () => { this.props.searchMovie(this.state.search, this.state.type, this.state.page); }
         );
     }
     render() {
+        let limit = 10;
+        let totalPages = Math.ceil(this.props.totalCount / limit);
+        const lastIndex = totalPages <= 10 ? totalPages + 1 : this.state.page + limit;
+        const firstIndex = totalPages <= 10 ? lastIndex - limit + lastIndex + 1 : lastIndex - limit;
+        let num = [];
+        for (let i = 0; i < totalPages + 1; i++) {
+            num.push(i);
+        }
         return (
             <div>
                 <div className="search">
@@ -37,7 +65,28 @@ class Seacrh extends React.Component {
                     <div><input type="radio" name="type" data-type="series" checked={this.state.type === 'series'} onChange={this.handleFilter} /> <span>Series</span></div>
                     <div><input type="radio" name="type" data-type="game" checked={this.state.type === 'game'} onChange={this.handleFilter} /> <span>Games</span></div>
                 </div>
-            </div>
+                <div className="navigation">
+                    <button className="btn" onClick={this.prevPage}>Previous</button>
+                    <div>
+                        {
+                            num.slice(firstIndex, lastIndex).map(
+                                (el, index) => (
+                                    <button
+                                        className="btn"
+                                        style={{ background: this.state.page !== el ? "grey" : "" }}
+                                        key={index}
+                                        // onСlick={() => this.setPage(el)}
+                                        onClick={() => this.setPage(el)}
+                                    >
+                                        {el}
+                                    </button>
+                                )
+                            )
+                        }
+                    </div>
+                    <button className="btn" onClick={this.nextPage}>Next</button>
+                </div>
+            </div >
 
         )
     }
--- src/layout/main/Main.js
+++ src/layout/main/Main.js
@@ -5,33 +5,46 @@ import Preloader from "../../components/preloader/Preloader";
 import Search from "../../components/search/Search";
 import Pagination from "../../components/pagination/Pagination";
 class Main extends React.Component {
-    state = { movies: [], loading: false, type: "all" };
+    state = { movies: [], loading: false, type: "all", count: 0 };
     componentDidMount() {
         fetch('https://omdbapi.com/?apikey=cbeeaf27&s=harry_potter')
             .then(response => response.json())
-            .then(data => this.setState({ movies: data.Search }));
+            .then(data => this.setState({ movies: data.Search, loading: false, count: data.totalResults }));
+        this.setState({ movies: [] });
+        // this.setState({ loading: true });
+        // fetch(`https://omdbapi.com/?apikey=cbeeaf27&s=harry_potter`)
+        //     .then(response => response.json())
+        //     .then(
+        //         data => {
+        //             if (data.Response === "True")
+        //                 this.setState({ movies: data.Search, loading: false, count: data.totalResults });
+        //             else
+        //                 this.setState({ movies: [], loading: false, count: data.totalResults });
 
+        //         });
     }
-    searchMovie = (str, type = 'all') => {
-        console.log(str, type)
+    searchMovie = (str, type = 'all', page) => {
         // this.setState({ movies: [] });
         this.setState({ loading: true });
-        fetch(`https://omdbapi.com/?apikey=cbeeaf27&s=${str.trim()}${type !== 'all' ? `&type=${type}` : ''}`)
+        fetch(`https://omdbapi.com/?apikey=cbeeaf27&s=${str.trim()}${type !== 'all' ? `&type=${type}` : ''}${`&page=${page}`}`)
             .then(response => response.json())
             .then(
                 data => {
                     if (data.Response === "True")
-                        this.setState({ movies: data.Search, loading: false });
+                        this.setState({ movies: data.Search, loading: false, count: data.totalResults });
                     else
-                        this.setState({ movies: [], loading: false });
+                        this.setState({ movies: [], loading: false, count: data.totalResults });
+
+                    console.log(`data: ${data.totalResults}`)
                 });
 
     }
     render() {
+
         return (
             <div className="main">
                 <div className="wrap">
-                    <Search searchMovie={this.searchMovie} />
+                    <Search searchMovie={this.searchMovie} totalCount={this.state.count} />
                     {
                         !this.state.loading && //this.state.movies.length &&
                         (this.state.movies.length ? <MovieList movies={this.state.movies} /> : <Preloader />)
